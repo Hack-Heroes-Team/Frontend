3f985c0dcf3dada5366a2d57f5d06023e3601126
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2022-10-12 00:24:09 +0200

    Set up and top bar

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+import { Image, Platform, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [search, onChangeSearch] = useState("");
+
+	return (
+		<ScrollView style={{ backgroundColor: "#fff" }}>
+			<SafeAreaView style={stylesTopBar.topBar}>
+				<View style={{ marginTop: 15, flexDirection: "row", justifyContent: "space-evenly" }}>
+					<View style={stylesTopBar.search}>
+						<Icon style={{ lineHeight: 37.5, color: "#fe2926" }} name="search" size={15} />
+						<TextInput style={{ paddingLeft: 10 }} onChangeText={onChangeSearch} value={search} placeholder="Wyszukaj..." />
+					</View>
+					<Image source={require("./assets/person-icon.png")} style={stylesTopBar.user} />
+				</View>
+			</SafeAreaView>
+		</ScrollView>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const stylesTopBar = StyleSheet.create({
+	topBar: {
+		backgroundColor: "#002047",
+		paddingTop: Platform.OS === "android" ? StatusBar.curentHeight : 0,
+	},
+	user: {
+		borderColor: "#fff",
+		borderWidth: 1,
+		width: 40,
+		height: 40,
+		borderRadius: "50%",
+	},
+	search: {
+		backgroundColor: "#fff",
+		marginBottom: 70,
+		height: 40,
+		width: "75%",
+		paddingHorizontal: 20,
+		flexDirection: "row",
+		color: "red",
+		borderRadius: "50%",
+	},
 });
