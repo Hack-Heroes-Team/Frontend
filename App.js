8a00a70d48e52f67027b224c54f4e6c5573a00e1
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2022-10-15 20:23:37 +0200

    New adding receipe screens

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Main from "./assets/pages/Main";
 import Account from "./assets/pages/Account";
 import AddingReceipt from "./assets/pages/AddingReceipt";
+import AddingReceiptManualy from "./assets/pages/AddingReceiptManualy";
 import ShopScreen from "./assets/pages/ShopScreen";
 import StartScreen from "./assets/pages/StartScreen";
 import LoginScreen from "./assets/pages/LoginScreen";
@@ -16,57 +17,68 @@ export default function App() {
 		<NavigationContainer>
 			<Stack.Navigator>
 				{loggedIn ? (
-					<>
-						<Stack.Group>
-							<Stack.Screen name="Home" component={Main} options={{ headerShown: false }} />
-							<Stack.Screen
-								name="Account"
-								component={Account}
-								options={{
-									headerStyle: {
-										backgroundColor: "#002047",
-									},
-									headerTitleStyle: {
-										fontWeight: "bold",
-									},
-									title: "Konto",
-									headerTintColor: "#fff",
-								}}
-							/>
-							<Stack.Screen
-								name="ShopScreen"
-								component={ShopScreen}
-								options={{
-									headerTintColor: "#fff",
-									headerTitleStyle: {
-										fontWeight: "bold",
-									},
-								}}
-							/>
-						</Stack.Group>
-						<Stack.Group screenOptions={{ presentation: "modal" }}>
-							<Stack.Screen
-								name="AddingReceipt"
-								component={AddingReceipt}
-								options={{
-									headerStyle: {
-										backgroundColor: "#002047",
-									},
-									headerTitleStyle: {
-										fontWeight: "bold",
-									},
-									headerTintColor: "#fff",
-									title: "Dodaj nowy pargon",
-								}}
-							/>
-						</Stack.Group>
-					</>
+					<Stack.Group>
+						<Stack.Screen name="Home" component={Main} options={{ headerShown: false }} />
+						<Stack.Screen
+							name="Account"
+							component={Account}
+							options={{
+								headerStyle: {
+									backgroundColor: "#002047",
+								},
+								headerTitleStyle: {
+									fontWeight: "bold",
+								},
+								title: "Konto",
+								headerTintColor: "#fff",
+							}}
+						/>
+						<Stack.Screen
+							name="ShopScreen"
+							component={ShopScreen}
+							options={{
+								headerTintColor: "#fff",
+								headerTitleStyle: {
+									fontWeight: "bold",
+								},
+							}}
+						/>
+
+						<Stack.Screen
+							name="AddingReceipt"
+							component={AddingReceipt}
+							options={{
+								headerStyle: {
+									backgroundColor: "#002047",
+								},
+								headerTitleStyle: {
+									fontWeight: "bold",
+								},
+								headerTintColor: "#fff",
+								title: "Dodaj nowy pargon",
+							}}
+						/>
+						<Stack.Screen
+							name="AddingReceiptManualy"
+							component={AddingReceiptManualy}
+							options={{
+								headerStyle: {
+									backgroundColor: "#002047",
+								},
+								headerTitleStyle: {
+									fontWeight: "bold",
+								},
+								headerTintColor: "#fff",
+								title: "Dodaj nowy pargon",
+							}}
+						/>
+					</Stack.Group>
 				) : (
-					<>
+					<Stack.Group>
 						<Stack.Screen name="StartScreen" component={StartScreen} options={{ headerShown: false }} />
 						<Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
 						<Stack.Screen name="RegisterScreen" component={RegisterScreen} options={{ headerShown: false }} />
-					</>
+					</Stack.Group>
 				)}
 			</Stack.Navigator>
 		</NavigationContainer>
---
+++ assets/pages/AddingReceiptManualy.js
@@ -0,0 +1,40 @@
+import { StyleSheet, Text, TouchableOpacity, View, TextInput } from "react-native";
+import { useState } from "react";
+
+import Icon from "react-native-vector-icons/Entypo";
+
+export default function AddingReceiptManualy({ navigation }) {
+	return (
+		<>
+			<TouchableOpacity onPress={() => navigation} style={styles.iconBackward}>
+				<Icon name="camera" style={{ color: "#fff" }} size={25} />
+			</TouchableOpacity>
+			<TouchableOpacity onPress={() => navigation} style={styles.iconForward}>
+				<Icon name="arrow-right" style={{ color: "#fff" }} size={25} />
+			</TouchableOpacity>
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+
+	iconBackward: {
+		backgroundColor: "#002047",
+		position: "absolute",
+		bottom: 37.5,
+		left: 10,
+		padding: 15,
+		borderRadius: 50,
+	},
+	iconForward: {
+		backgroundColor: "#002047",
+		position: "absolute",
+		bottom: 37.5,
+		right: 10,
+		padding: 15,
+		borderRadius: 50,
+	},
+});
